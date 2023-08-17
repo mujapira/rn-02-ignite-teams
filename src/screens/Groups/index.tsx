93108ead4929688c8e367962b2a3a4f459c930c0
mujapira <malmasmello2@gmail.com>
2023-08-17 14:22:37 -0300

    Section Header Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,15 @@
 import { Header } from '@components/Header';
 import * as S from './styles';
+import { SectionHeader } from '@screens/SectionHeader';
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
-      <S.Title>Grupos a</S.Title>
+      <SectionHeader 
+        title="Turmas"
+        subtitle="jogue com a sua turma"
+      />
     </S.Container>    
   );
 }
