ff64c7c2d51528d227ceb42d6a5aa5c17139485e
mujapira <malmasmello2@gmail.com>
2023-08-17 13:22:48 -0300

    loading and status bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import {ThemeProvider} from 'styled-components'
 import theme from './src/theme'
 import { Groups } from './src/screens/Groups';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto"
+import { Loading } from '@components/loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+  
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
