7dbb09e64d199ddb02cbed8b8490c41a2bd9702b
mujapira <malmasmello2@gmail.com>
2023-08-17 14:39:15 -0300

    Listing groups and EmptyGroups component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,7 +4,6 @@ import { CaretLeft } from 'phosphor-react-native'
 
 
 export const Container = styled.View`
-  padding-top: 12px;
   width: 100%;
   flex-direction: row;
   align-items: center;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,13 @@ import { Header } from '@components/Header';
 import * as S from './styles';
 import { SectionHeader } from '@components/SectionHeader';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/EmptyList';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <S.Container>
       <Header />
@@ -11,8 +16,19 @@ export function Groups() {
         title="Turmas"
         subtitle="jogue com a sua turma"
       />
-       <GroupCard 
-        title="Galera do Ignite" />
+       <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </S.Container>    
   );
 }
